2ded2a912a7ab8df734221eff3436348b165f532
Marc_M <marcmartinez987@gmail.com>
2020-07-07 18:38:51 -0700

    initial display of products added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,5 +12,20 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log(connection.threadId);
-    //displayProducts();
+    displayProducts();
 });
+
+function displayProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if(err) throw err;
+        // console.log(res);
+        console.log("-----------------------");
+        console.log("Welcome To Bamazon");
+        console.log("-----------------------");
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity + " | ");
+
+        };
+        //start();
+    });
+};
