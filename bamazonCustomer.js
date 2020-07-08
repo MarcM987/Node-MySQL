d8dff51f26825ec5f329eb30049dcc148d6747fb
Marc_M <marcmartinez987@gmail.com>
2020-07-07 19:18:12 -0700

    console table used instead of log, need to remove or replace index col

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,20 +12,29 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("Connection ThreadID: " + connection.threadId);
-    displayProducts();
+    display();
 });
 
-function displayProducts() {
+function display() {
     connection.query("SELECT * FROM products", function (err, res) {
         if(err) throw err;
         // console.log(res);
+        console.log("\n\n-----------------------");
+        console.log("Products for sale");
         console.log("-----------------------");
-        console.log("Welcome To Bamazon");
-        console.log("-----------------------");
-        for (var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity + " | ");
+        // console.log("ID | Product | Department | Price | Stock");
+        var products = [{}];
+        for (let i = 0; i < res.length; i++) {
+            // console.log(res[i].item_id + " | " 
+            // + res[i].product_name + " | " 
+            // + res[i].department_name + " | " 
+            // + res[i].price + " | " 
+            // + res[i].stock_quantity + " | ");
+
+            products.push({ID: res[i].item_id, Item: res[i].product_name, Dept: res[i].department_name, Price: res[i].price, Stock: res[i].stock_quantity});
 
         };
+        console.table(products);
         //start();
     });
 };
